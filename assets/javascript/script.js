28edd466eb405d60fe86dfd555589933ba66064e
Tucker <tuckerk23@gmail.com>
2017-12-10 13:49:50 -0700

    added some logic

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,28 +1,70 @@
-// var words = ["this", "that", "some", "words"];
-var words = {
-    1: "launch",
-    2: "orbit",
-    3: "station",
-    4: "houston",
-    5: "nasa"
-};
+var champions = [
+  "Ashe",
+  "Caitlyn",
+  "Corki",
+  "Draven",
+  "Ezreal",
+  "Graves",
+  "Jhin",
+  "Jinx",
+  "Kalista",
+  "Kindred",
+  "Kogmaw",
+  "MissFortune",
+  "Quinn",
+  "Sivir",
+  "Tristana",
+  "Twitch",
+  "Varus",
+  "Vayne",
+  "Xayah"
+];
 
-for (var key in words){
-    console.log("The word is "+ words[key]);
+var chosenWord = "";
+var letterGuessed = [];
+var displayArray = [];
+var wordLetters = [];
+var wrongLetters = [];
 
+var wins = 0;
+var losses = 0;
+var guessLimit = 10;
 
-var lettersGuessed = [];
+function begin() {
+  //sets guess limit to 10 in the beginning and in new instances
+  guessLimit = 10;
+  //clears out display for new instance
+  displayArray = [];
+  //clears out the letters guessed for new instance
+  wrongLetters = [];
 
+  //chooses a random word from the array
+  chosenWord = champions[Math.floor(Math.random() * champions.length)];
 
-document.onkeyup = function(event){
-    var userChoice = event.keyCode();
-    
-    if(userChoice == words[key].charAt()){
-    console.log("correct guess");
-    } else {
-        lettersGuessed.push(userChoice);
-        console.log("wrong guess");
-    }
-};
+  console.log(chosenWord);
 
+  //separates the chosen word letters into individual strings
+  wordLetters = chosenWord.split("");
+
+  //loops through chosen word to display underscores for each digit
+  for (var i = 0; i < chosenWord.length; i++) {
+    displayArray.push("_");
+  }
+  console.log(displayArray);
 }
+
+function wordLogic(x) {
+  var inWord = false;
+  for (i = 0; i < chosenWord; i++) {
+    if (chosenWord[i] === x) {
+        inWord = true;
+    }
+  }
+}
+
+begin();
+
+document.onkeyup = function(event) {
+  letterGuessed = String.fromCharCode(event.keyCode).toLowerCase;
+  wordLogic();
+};
