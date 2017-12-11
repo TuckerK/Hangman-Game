cfd13d4fcc2f4a000ec9a6248403900217e36e9b
Tucker King <tuckerk23@gmail.com>
2017-12-10 21:01:55 -0700

    finished up to my capabilty

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,23 +1,23 @@
 var champions = [
-  "Ashe",
-  "Caitlyn",
-  "Corki",
-  "Draven",
-  "Ezreal",
-  "Graves",
-  "Jhin",
-  "Jinx",
-  "Kalista",
-  "Kindred",
-  "Kogmaw",
-  "MissFortune",
-  "Quinn",
-  "Sivir",
-  "Tristana",
-  "Twitch",
-  "Varus",
-  "Vayne",
-  "Xayah"
+  "ashe",
+  "caitlyn",
+  "corki",
+  "draven",
+  "ezreal",
+  "graves",
+  "jhin",
+  "jinx",
+  "kalista",
+  "kindred",
+  "kogmaw",
+  "missfortune",
+  "quinn",
+  "sivir",
+  "tristana",
+  "twitch",
+  "varus",
+  "vayne",
+  "xayah"
 ];
 
 var chosenWord = "";
@@ -25,46 +25,84 @@ var letterGuessed = [];
 var displayArray = [];
 var wordLetters = [];
 var wrongLetters = [];
+var underscores = 0;
 
 var wins = 0;
 var losses = 0;
 var guessLimit = 10;
 
 function begin() {
-  //sets guess limit to 10 in the beginning and in new instances
   guessLimit = 10;
-  //clears out display for new instance
   displayArray = [];
-  //clears out the letters guessed for new instance
   wrongLetters = [];
-
-  //chooses a random word from the array
   chosenWord = champions[Math.floor(Math.random() * champions.length)];
 
   console.log(chosenWord);
 
-  //separates the chosen word letters into individual strings
   wordLetters = chosenWord.split("");
 
-  //loops through chosen word to display underscores for each digit
+  displayArray.join("%");
+
+  underscores = wordLetters.length;  
+  document.getElementById("directions").innerHTML = "Press some letters, Summoner!";
+
   for (var i = 0; i < chosenWord.length; i++) {
-    displayArray.push("_");
+    displayArray.push(" _ ");
   }
   console.log(displayArray);
+  document.getElementById("wrongLetters").innerHTML = wrongLetters;
+  
+  document.getElementById("displayArray").innerHTML = displayArray;
+  
 }
 
 function wordLogic(x) {
   var inWord = false;
-  for (i = 0; i < chosenWord; i++) {
+  for (var i = 0; i < underscores; i++) {
     if (chosenWord[i] === x) {
-        inWord = true;
+      inWord = true;
     }
+  }
+  if(inWord){
+  for (var j = 0; j < underscores; j++){
+    if (chosenWord[j] === x){
+      displayArray[j] = x;
+    }
+  }
+  console.log(displayArray);
+} else {
+  wrongLetters.push(x);
+  document.getElementById("wrongLetters").innerHTML = wrongLetters + "";  
+  guessLimit--;
+}
+document.getElementById("displayArray").innerHTML = displayArray;
+}
+
+function end(){
+  if(wordLetters.toString() === displayArray.toString()){
+    wins++;
+    begin();
+    document.getElementById("wins").innerHTML = "Wins: " + wins;
+    document.getElementById("directions").innerHTML = "You Win! Try Again!";
+    document.getElementById("victory-audio").play();      
+    
+    
+  }else if(guessLimit == 0){
+    losses++;
+    begin();
+    document.getElementById("losses").innerHTML = "Losses: " + losses;
+    document.getElementById("directions").innerHTML = "You Lose! Try Again!";
+    document.getElementById("defeat-audio").play();  
+    
   }
 }
 
 begin();
 
 document.onkeyup = function(event) {
-  letterGuessed = String.fromCharCode(event.keyCode).toLowerCase;
-  wordLogic();
+  letterGuessed = String.fromCharCode(event.keyCode).toLowerCase("");
+  document.getElementById("directions").innerHTML = "Press some letters, Summoner!";    
+  wordLogic(letterGuessed);
+  end();
+  
 };
